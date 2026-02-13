231009df2322770e725485249ccba379c51a5bac
LostPacket <84755549+sinkobobux@users.noreply.github.com>
2026-02-13 23:18:57 +0545

    Extract urls for relevant articles checking the anchor tag texts

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+urls=[f"https://kathmandupost.com/","https://thehimalayantimes.com/","https://english.onlinekhabar.com/"]
+per_site=[]
+suburls=[]
+
+trigger_words=["election","elections","party","rsp","uml","congress"]
+
+for url in urls:
+    response=requests.get(url)
+    content = BeautifulSoup(response.text,"html.parser")
+    for link in content.find_all('a'):
+        if any(t_word in link.text.lower() for t_word in trigger_words):
+            #print (link.text)
+            per_site.append(link.get('href'))
+            if len(per_site)>3:
+                break
+    suburls.append(per_site)
+
+
+print(suburls)
