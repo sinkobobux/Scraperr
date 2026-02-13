747a2eaf06c7cc2266d7e32dd40f63c9ad727db5
LostPacket <84755549+sinkobobux@users.noreply.github.com>
2026-02-13 23:31:34 +0545

    Convert relative links to absolute urls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 urls=[f"https://kathmandupost.com/","https://thehimalayantimes.com/","https://english.onlinekhabar.com/"]
 per_site=[]
@@ -13,7 +14,7 @@ for url in urls:
     for link in content.find_all('a'):
         if any(t_word in link.text.lower() for t_word in trigger_words):
             #print (link.text)
-            per_site.append(link.get('href'))
+            per_site.append(urljoin(url,link.get('href')))
             if len(per_site)>3:
                 break
     suburls.append(per_site)
