d8b4207d047da2efff4851a289d1608178eb781c
LostPacket <84755549+sinkobobux@users.noreply.github.com>
2026-02-14 20:56:49 +0545

    Pack headline with url as tuple for writing to file and make some variables local

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 
 urls=[f"https://kathmandupost.com/",f"https://thehimalayantimes.com/",f"https://english.onlinekhabar.com/"]
-per_site=[]
-suburls=[]
-text=[]
 avoid=["tag/","opinion/"]
 trigger_words=["election","elections","party"]
 output_dir="extracted_stuff"
@@ -14,6 +11,8 @@ if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
 def get_url():
+    per_site=[]
+    links=[]
     for url in urls:
         per_site.clear()
         response=requests.get(url)
@@ -23,29 +22,29 @@ def get_url():
                 #print (link.text)
                 absolute_url = urljoin(url,link.get('href'))
                 if absolute_url not in per_site and all(avoid_path not in urlparse(absolute_url).path for avoid_path in avoid):
-                    print(absolute_url)
-                    per_site.append(absolute_url)
+                    per_site.append((link.text,absolute_url))
                 if len(per_site)>2:
                     break
-        suburls.append(per_site.copy())
+        links.append(per_site.copy())
+    return links
 
-
-def get_body():
-    #print(suburls)
-    for site in suburls:
-        for i,url in enumerate(site):
+def get_body(links):
+    text=[]
+    for site in links:
+        for i,(headline,url) in enumerate(site):
             text.clear()
             response=requests.get(url)
             content=BeautifulSoup(response.text,"html.parser").find_all('p')
             for paragraph in content:
                 text.append(paragraph)
             with open(f"{output_dir}/{urlparse(url).netloc}-{i}.txt",'w') as f:
+                f.write(headline.strip()+"\n\n")
                 for paragraph in text:
                     f.write(paragraph.get_text(strip=True))
 
 def main():
-    get_url()
-    get_body()
+    links=get_url()
+    get_body(links)
 
 
 if __name__=="__main__":
